f9716fd5078f9af892934847e38a5291c7590b21
MineDeb <csglobaloffensiveupgrade@gmail.com>
2024-03-20 23:22:27 +0300

    few changes

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.header__button');
-const menu = document.querySelector('.header__adaptive');
+const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
-  menu.classList.toggle('red');
+  menu.classList.toggle('visible');
 });

78b446b56432483fecd579b62cd28829446e1a0b
MineDeb <csglobaloffensiveupgrade@gmail.com>
2024-03-20 12:51:36 +0300

    add button some functionality

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.header__button');
+const menu = document.querySelector('.header__adaptive');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
-  
+  menu.classList.toggle('red');
 });
